bced0a57f42ed3e593dc7a4b203c537348b086b2
KemalGokten <ukemalgokten@gmail.com>
2024-04-23 18:12:54 +0200

    Add ref to cohort field to reference Cohort Collection

--- server/models/student.model.js
+++ server/models/student.model.js
@@ -10,7 +10,7 @@ const studentSchema = new Schema({
     program:{type:String, enum:["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]},
     background:{type:String, default:""},
     image:{type:String,default:"https://i.imgur.com/r8bo8u7.png"},
-    cohort:Schema.Types.ObjectId,
+    cohort:{type:Schema.Types.ObjectId, ref:"Cohort"},
     projects:Array
 })
 

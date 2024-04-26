0fdca2706c747353c1bc46ba521ee76a49e5cf9f
KemalGokten <ukemalgokten@gmail.com>
2024-04-26 13:47:57 +0200

    Change username prop to userName

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
-    username: { type: String, required: true, unique: true, trim: true },
+    userName: { type: String, required: true, unique: true, trim: true },
     email: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true, unique: true },
   },
